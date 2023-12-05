2cfdca08344ec18d390280e7d4d6bdafb643af09
beffyd <beth._@hotmail.co.uk>
2023-12-05 15:35:54 +0000

    saving

---
+++ javascript/loop.js
@@ -0,0 +1,10 @@
+/* CODE DEMONSTRATING FOR LOOP */
+
+let multiplyBy = prompt("Please Eenter a number: ")
+
+for ( counter = 1 ; counter <=10 ; counter = counter + 1 )
+{
+    document.write(counter);
+    document.write(counter, "<br>",)
+    document.write(counter * 6 );
+}
